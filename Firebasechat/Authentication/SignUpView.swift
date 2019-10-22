0584356235b50f1982035645b468e293f964bfd8
Antonio Vega <antoniovegajr@Antonios-MacBook-Pro.local>
2019-10-21 17:15:20 -0700

    first commit

---
+++ Firebasechat/Authentication/SignUpView.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpView.swift
+//  Firebasechat
+//
+//  Created by Mark Kinoshita on 10/9/19.
+//  Copyright © 2019 Mark Kinoshita. All rights reserved.
+//
+
+import UIKit
+
+class SignUpView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
